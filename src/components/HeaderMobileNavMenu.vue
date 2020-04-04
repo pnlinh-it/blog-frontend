8cd7ec0c5628aff1a49efb0862808b2fbb9c442c
Phạm Linh <pnlinh.it@gmail.com>
2020-04-04 13:00:55 +0700

    Change header when screen width change

--- src/components/HeaderMobileNavMenu.vue
+++ src/components/HeaderMobileNavMenu.vue
@@ -3,31 +3,16 @@
     <div class="container mx-auto pt-4 px-4">
         <vs-card>
             <div class="flex items-center">
-                <vs-icon icon="reorder" @click="isSideBarOpen = !isSideBarOpen" size="small" color="#626262"
+                <vs-icon icon="reorder" @click.stop="$emit('onOpenSideBarClick')" size="small" color="#626262"
                          class="cursor-pointer p-0 ml-2"/>
                 <span class="mx-auto">Hi there</span>
             </div>
         </vs-card>
-        <vs-sidebar parent="body" color="primary" spacer v-model="isSideBarOpen">
-            <vertical-nav-menu/>
-        </vs-sidebar>
     </div>
 </template>
 
 <script>
-    import VerticalNavMenu from "@/components/VerticalNavMenu";
-
-    export default {
-        name: "HeaderMobileMavMenu",
-        components: {
-            VerticalNavMenu
-        },
-        data() {
-            return {
-                isSideBarOpen: false
-            }
-        }
-    }
+    export default {}
 </script>
 
 <style>
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,6 +2,11 @@ const mutations = {
     UPDATE_WINDOW_SCROLL_Y(state, scrollY) {
         state.scrollY = scrollY
     },
-
+    UPDATE_WINDOW_WIDTH(state, width) {
+        state.screenWidth = width
+    },
+    UPDATE_SIDE_BAR_STATUS(state, isOpen) {
+        state.isSideBarOpen = isOpen
+    },
 }
 export default mutations
